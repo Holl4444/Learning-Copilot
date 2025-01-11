4b1d34ac525cac0315854a953abcbcc6642ccede
Holl4444 <162161116+Holl4444@users.noreply.github.com>
2025-01-11 21:02:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+    var members = [
+        {name: 'John', skills: ['JavaScript', 'React', 'Node']},
+        {name: 'Anna', skills: ['JavaScript', 'React', 'Node']},
+        {name: 'Mary', skills: ['JavaScript', 'Angular', 'Node']}
+    ];
+    return members;
+}
